88bc180665d56ffd1681fc0fd05dd9943b385207
Karl Palsson <karlp@tweak.net.au>
2014-10-22 00:31:40 +0000

    Intial commit. logic traces show writes work at least.
    No status reporting of nack/ack, no reads, nothing actually useful yet :)

---
+++ README.md
@@ -0,0 +1,8 @@
+pyusb wrappers for using a ch341 in i2c mode.
+
+Somewhat based on git@github.com:commandtab/ch341eeprom.git but more openended,
+and not feeling the need to do everything in C.
+
+usb2i2c: https://code.google.com/p/usb2i2c/ seems to be identical protocols,
+possibly earlier versions of the same chips. (And there's code available!
+whee!)
---
+++ quick.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# Karl Palsson, October 2014 <karlp@tweak.net.au>
+# Considered to be released under your choice of MIT/Apache2/BSD 2 clause
+#
+# Provides generic hooks for reading/writing i2c via a CH341 in i2c/mem/epp
+# mode.  Has only been _currently_ tested with a CH341A device.
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("py2c-ch34x")
+
+import struct
+import usb.core
+import usb.util
+
+# These definitions are from ch341eeprom, and from ch34x vendor serial uart driver
+# They're all just magic numbers, so talk to your lawyers about who owns what
+# spellings of the names.  (No debate that work was done by people to recover these
+# And that was not all me)
+# Oh, and it's all the same as https://code.google.com/p/usb2i2c
+# the definitions are the same!
+magic_read_setup = "\xaa\x74\x83\xa0\x00\x00\x74\x81\xa1\xe0\x00\x00\x06\x04\x00\x00" \
+            "\x00\x00\x00\x00\x40\x00\x00\x00\x11\x4d\x40\x77\xcd\xab\xba\xdc" \
+            "\xaa\xe0\x00\x00\xc4\xf1\x12\x00\x11\x4d\x40\x77\xf0\xf1\x12\x00" \
+            "\xd9\x8b\x41\x7e\x00\xf0\xfd\x7f\xf0\xf1\x12\x00\x5a\x88\x41\x7e" \
+            "\xaa\xe0\x00\x00\x2a\x88\x41\x7e\x06\x04\x00\x00\x11\x4d\x40\x77" \
+            "\xe8\xf3\x12\x00\x14\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00" \
+            "\xaa\xdf\xc0\x75\x00"
+
+class CtrlCommands():
+    """
+    This is just usb standard stuff...
+    """
+    WRITE_TYPE = 0x40
+    READ_TYPE = 0xc0
+
+class VendorCommands():
+    READ_REG = 0x95
+    WRITE_REG = 0x9a
+    SERIAL = 0xa1
+    PRINT = 0xa3
+    MODEM = 0xa4
+    SPI = 0xa8
+    SIO = 0xa9
+    I2C = 0xaa
+    UIO = 0xab
+    VERSION = 0x5f # at least in serial mode?
+
+class I2CCommands():
+    # These are all from ch341dll.h, mostly untested
+    STA = 0x74
+    STO = 0x75
+    OUT = 0x80
+    IN = 0xc0
+    MAX = 32 # min (0x3f, 32) ?!
+    SET = 0x60 # 0 = 20
+    US = 0x40
+    MS = 0x50
+    DLY = 0x0f
+    END = 0x00 # Finish commands with this. is this really necessary?
+
+
+class CH341():
+    def vendor_read(self, req, wValue, wIndex, len):
+        return self.dev.ctrl_transfer(CtrlCommands.READ_TYPE, req, wValue, wIndex, len)
+
+    def __init__(self, vid=0x1a86, pid=0x5512):
+        dev = usb.core.find(idVendor=vid, idProduct=pid)
+        if not dev:
+            raise ValueError("Device not found (%x:%x" % (vid, pid))
+        log.info("Found device (%x:%x) version: %d.%d",
+                 vid, pid, dev.bcdDevice >> 8, dev.bcdDevice & 0xff)
+        # These devices only have one that I know of...
+        assert(dev.bNumConfigurations == 1)
+        dev.set_configuration()
+        self.dev = dev
+        # i2c vs epp vs mem mode? or is this fixed?
+        log.debug("Device protocol? %d", dev.bDeviceProtocol)
+
+        #ch34x_vendor_read( VENDOR_VERSION, 0x0000, 0x0000, serial, buf, 0x02 );
+        #static int ch34x_vendor_read( __u8 request,__u16 value,  __u16 index,
+        #                struct usb_serial *serial, unsigned char *buf, __u16 len )
+        #retval = usb_control_msg( serial->dev, usb_rcvctrlpipe(serial->dev, 0),
+        #                request, VENDOR_READ_TYPE, value, index, buf, len, 1000 );
+        vv = self.vendor_read(VendorCommands.VERSION, 0, 0, 2)
+        log.info("vendor version = %d.%d (%x.%x)", vv[0], vv[1], vv[0], vv[1])
+
+    def set_speed(self, speed=100):
+        """
+        Set the i2c speed desired
+        :param speed: in khz, will round down to 20, 100, 400, 750
+        :return: na
+        """
+        sbit = 1
+        if speed < 100:
+            sbit = 0
+        elif speed < 400:
+            sbit = 1
+        elif speed < 750:
+            sbit = 2
+        else:
+            sbit = 3
+
+        # TODO ^ how does linux handle this sort of stuff normally?
+        # logging when it doesn't match?
+        cmd = [VendorCommands.I2C, I2CCommands.SET | sbit, I2CCommands.END]
+        count = self.dev.write(2, cmd)
+        assert count == len(cmd), "Failed to write cmd to usb"
+
+    def eeprom_read(self, address, start, count):
+        """
+        Issue an i2c read (single byte addressing, 24cxx styleee)
+        :param address: the i2c address of the eeprom
+        :param start: the starting address to read from
+        :param count: how many bytes to read
+        :return:
+        """
+        # HACK ALERT - still decodign the i2c read/write stuff
+        if start > 0x7ff:
+            raise ValueError("Can't handle > 16k devices yet")
+        len_write = 2 # for 24c01/24c02, address is one byte
+        # for up to 16k, this fits in
+        eep_cmd = [address | (start >> 7) & 0x0e, (start & 0xff)]
+        cmd = [VendorCommands.I2C,
+               I2CCommands.STA,
+               I2CCommands.OUT | len(eep_cmd),
+               ] + eep_cmd
+        log.debug("writing: %s", cmd)
+        self.dev.write(2, cmd)
+        # TODO - need to read in now :)
+
+
+
+if __name__ == "__main__":
+    q = CH341()
+    q.set_speed(100)
+    x = q.eeprom_read(0x40, 0, 4)
+    x = q.eeprom_read(0x40, 0x55, 4)
+    log.info("received: %s", x)
